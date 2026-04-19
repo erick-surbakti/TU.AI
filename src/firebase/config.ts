ed05558ed17901c3a364d0d19b89a3d488e74dc9
Erick Surbakti <ericksurbaktii@gmail.com>
2026-04-19 09:20:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3361630461-52f5d",
+  "appId": "1:832347415618:web:938ff5e212f000aedab9db",
+  "apiKey": "AIzaSyB4OKqxwc3HsKVpl2uzCH7niw4LAgwnzfo",
+  "authDomain": "studio-3361630461-52f5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "832347415618"
+};
